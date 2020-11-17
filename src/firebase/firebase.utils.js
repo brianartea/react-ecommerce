fa69582cd7da57c1aea961cd67547236721e682c
brianartea <bri@expertdesign.cc>
2020-11-17 12:07:42 -0500

    finishing signin

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-var firebaseConfig = {
+const config = {
   apiKey: "AIzaSyB5u31nVd3-5QN25TGJYFZvhukjUSjP4c4",
   authDomain: "ecom-11-16-20.firebaseapp.com",
   databaseURL: "https://ecom-11-16-20.firebaseio.com",
@@ -12,15 +12,39 @@ var firebaseConfig = {
   appId: "1:390568102468:web:f7f6972ce5395301318a11",
   measurementId: "G-5WGSRBZ7QH",
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
-//export const firebase = firebase.firestore();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
-
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
